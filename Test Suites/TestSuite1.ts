91fb56fd0024cc92e46b0ebbb34ea8bff6853fce
isont <isont@192.168.20.6>
2018-12-16 20:33:15 +1300

    First Project commit

---
+++ Test Suites/TestSuite1.ts
@@ -0,0 +1,38 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TestSuite1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <lastRun>2018-12-16T19:30:43</lastRun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>5734e496-1f22-4388-8dfa-9ca27b5f9998</testSuiteGuid>
+   <testCaseLink>
+      <guid>54a6143f-8f50-4204-893b-4929825d04db</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Caclulator/Addition</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>0f577633-60fa-4120-a6ef-e4d43bbafdbf</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/GetBankDetails</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>2cbda864-47ad-4a42-889d-81428c4c1fb8</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/HttpMethods</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>5ccad591-13ce-4c90-8c6a-d0d9ab5573dc</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/ListUsersTest</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
